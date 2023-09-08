179dcefb6e865215ebe579b0c5e977e2090834c3
Jacob Casas <skedabble@gmail.com>
2023-09-08 14:24:56 -0700

    background and style changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         $('.borderr').css("padding-top", "0");
         $('.title').css("padding-top", "25px");
         $('.title-0, .title-2').empty();
-        $('.main-container').css("background-color", "black");
+        $('.main-container').css("background-color", "rgba(0,0,0,0.93);");
         $('.main-container').css("padding-bottom", "30px");
         $('.main-container').css("padding-left", "15px");
         $('.main-container').css("padding-right", "15px");
@@ -34,27 +34,26 @@ $(document).ready(function () {
         $.ajax(settings).done(function (response) {
             $('.title').append(response.results[0].name);
             $('.feedImg').attr("src", response.results[0].picture);
-            $('.main-container').append('<li style="margin-top: 20px; margin-bottom: 20px;">' + response.results[1].locations[0].display_name + '</li>');
+            $('.main-container').append('<li class="li-transform" style="margin-top: 20px; margin-bottom: 20px;">' + response.results[1].locations[0].display_name + '</li>');
             $('.main-container').append('<a target="_blank" class="url-6">Watch Now</a>');
             $('.url-6').attr('href', response.results[1].locations[0].url);
-            $('.main-container').append('<li style="margin-top: 20px; margin-bottom: 20px;">' + response.results[0].locations[0].display_name + '</li>');
+            $('.main-container').append('<li class="li-transform" style="margin-top: 20px; margin-bottom: 20px;">' + response.results[0].locations[0].display_name + '</li>');
             $('.main-container').append('<a target="_blank" class="url-1">Watch Now</a>');
             $('.url-1').attr("href", response.results[0].locations[0].url);
-            $('.main-container').append('<li style="margin-top: 20px; margin-bottom: 20px;">' + response.results[0].locations[1].display_name + '</li>');
+            $('.main-container').append('<li class="li-transform" style="margin-top: 20px; margin-bottom: 20px;">' + response.results[0].locations[1].display_name + '</li>');
             $('.main-container').append('<a target="_blank" class="url-2">Watch Now</a>');
             $('.url-2').attr('href', response.results[0].locations[1].url);
-            $('.main-container').append('<li style="margin-top: 20px; margin-bottom: 20px;">' + response.results[0].locations[2].display_name + '</li>');
+            $('.main-container').append('<li class="li-transform" style="margin-top: 20px; margin-bottom: 20px;">' + response.results[0].locations[2].display_name + '</li>');
             $('.main-container').append('<a target="_blank" class="url-3">Watch Now</a>');
             $('.url-3').attr('href', response.results[0].locations[2].url);
-            $('.main-container').append('<li style="margin-top: 20px; margin-bottom: 20px;">' + response.results[0].locations[3].display_name + '</li>');
+            $('.main-container').append('<li class="li-transform" style="margin-top: 20px; margin-bottom: 20px;">' + response.results[0].locations[3].display_name + '</li>');
             $('.main-container').append('<a target="_blank" class="url-4">Watch Now</a>');
             $('.url-4').attr('href', response.results[0].locations[3].url);
-            $('.main-container').append('<li style="margin-top: 20px; margin-bottom: 20px;">' + response.results[0].locations[4].display_name + '</li>');
+            $('.main-container').append('<li class="li-transform" style="margin-top: 20px; margin-bottom: 20px;">' + response.results[0].locations[4].display_name + '</li>');
             $('.main-container').append('<a target="_blank" class="url-5" style="margin-bottom:25px;">Watch Now</a>');
             $('.url-5').attr('href', response.results[0].locations[4].url);
         });
     })
-
 });
 
     $('.mr-sm-2').keypress(function (e) {
